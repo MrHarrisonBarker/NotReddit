683100b6d684546b1ae652781f414cf49f09dadf
Harrison Barker <harrison@thebarkers.me.uk>
2018-09-28 00:48:28 +0100

    rm

--- NotRedditApp/src/app/post.service.ts
+++ NotRedditApp/src/app/post.service.ts
@@ -3,7 +3,6 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
 
 import {Post} from './post';
-import {catchError} from "rxjs/operators";
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
