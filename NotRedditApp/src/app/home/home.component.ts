4497adf51fb4c933e28e2873ff8bdc074d86e6ba
Harrison Barker <harrison@thebarkers.me.uk>
2018-10-06 02:16:45 +0100

    Added dropdown menu and sorting functionality

--- NotRedditApp/src/app/home/home.component.ts
+++ NotRedditApp/src/app/home/home.component.ts
@@ -21,10 +21,49 @@ export class HomeComponent implements OnInit {
   }
 
   getAllPosts() {
-    this.postService.getAllPosts().subscribe(posts => this.posts = posts);
+    this.postService.getAllPosts().subscribe(posts => {
+      this.posts = posts.sort(function (a, b) {
+        const titleA = a.createdOn;
+        const titleB = b.createdOn;
+
+        let comparison = 0;
+        if (titleA > titleB) {
+          comparison = 1;
+        } else if (titleA < titleB) {
+          comparison = -1;
+        }
+        return comparison;
+      })
+    });
   }
 
   changeContainer() {
     this.isFluid = this.isFluid ? false : true;
   }
+
+  orderPosts(property, isAscending) {
+    this.posts.sort(function (a, b) {
+      const A = a[property];
+      const B = b[property];
+
+      let comparison = 0;
+      if (isAscending) {
+        if (A > B) {
+          comparison = 1;
+        } else if (A < B) {
+          comparison = -1;
+        }
+        return comparison;
+      } else {
+        if (A < B) {
+          comparison = 1;
+        } else if (A > B) {
+          comparison = -1;
+        }
+        return comparison;
+      }
+
+    })
+  }
+
 }
