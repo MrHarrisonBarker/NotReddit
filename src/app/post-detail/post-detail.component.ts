6331376c1a1e279286e97111a6e2c66a69547272
Harrison Barker <harrison@thebarkers.me.uk>
2018-10-09 16:45:18 +0100

    Post detail component worked on

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,43 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import {PostService} from "../post.service";
-import {ActivatedRoute} from "@angular/router";
-import {Location} from "@angular/common";
-import {Post} from "../post";
+import {Component, OnInit} from '@angular/core';
+import {PostService} from '../post.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+import {Post} from '../post';
+import {GlobalsService} from '../globals.service';
 
 @Component({
-  selector: 'app-post-detail',
-  templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.scss']
+    selector: 'app-post-detail',
+    templateUrl: './post-detail.component.html',
+    styleUrls: ['./post-detail.component.scss']
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor(private postService: PostService,
-              private route: ActivatedRoute,
-              private location: Location) { }
+    constructor(private postService: PostService,
+                private route: ActivatedRoute,
+                private location: Location,
+                public globals: GlobalsService) {
+    }
 
-  selectedPost: Post;
+    selectedPost: Post;
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      console.log(params['_id']);
-      this.getPost(params['_id']);
-    });
-  }
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            console.log(params['_id']);
+            this.getPost(params['_id']);
+        });
+    }
 
-  getPost(id) {
-    console.log(id);
-    this.postService.getPost(id)
-      .subscribe( data => this.selectedPost = data ,
-        error => console.log(error));
-  }
+    getPost(id) {
+        console.log(id);
+        this.postService.getPost(id)
+            .subscribe(data => this.selectedPost = data,
+                error => console.log(error));
+    }
 
-  removePost(post: Post) {
-    this.postService.deletePost(post);
-    console.log(post);
-  }
+    removePost(post: Post) {
+        this.postService.deletePost(post);
+        console.log(post);
+    }
 
-  goBack(): void {
-    this.location.back();
-  }
+    goBack(): void {
+        this.location.back();
+    }
+
+    changeContainer() {
+        this.globals.isFluid = this.globals.isFluid ? false : true;
+    }
+
+    changeMode() {
+        this.globals.isDark = this.globals.isDark ? false : true;
+        console.log(this.globals.isDark);
+    }
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit, NgZone} from '@angular/core';
-import { Post } from '../post';
+import {Post} from '../post';
 import {PostService} from '../post.service';
 import {DomainService} from '../domain.service';
 import {Domain} from '../domain';
@@ -8,60 +8,60 @@ import {GlobalsService} from '../globals.service';
 // TODO: Crosspost
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
 
-  @Input() post: Post;
-  domain: Domain;
-  voteStatus: String;
-  hasVoteChanged: Boolean;
+    @Input() post: Post;
+    domain: Domain;
+    voteStatus: String;
+    hasVoteChanged: Boolean;
 
-  constructor(private postService: PostService,
-              private domainService: DomainService,
-              public globals: GlobalsService) {
-  }
-
-
-  ngOnInit() {
-    console.log(this.post);
-    this.getDomain(this.post.Domain);
-  }
-
-  updateRank(post: Post, up: boolean) {
-    if (up) {
-      if (this.hasVoteChanged){
-        post.Rank += 1;
-        this.hasVoteChanged = false;
-        this.voteStatus = 'black';
-      } else {
-        post.Rank += 1;
-        this.hasVoteChanged = true;
-        this.voteStatus = 'green';
-      }
-    } else {
-      if (this.hasVoteChanged) {
-        post.Rank -= 1;
-        this.hasVoteChanged = false;
-        this.voteStatus = 'black';
-      } else {
-        post.Rank -= 1;
-        this.hasVoteChanged = true;
-        this.voteStatus = 'red';
-      }
-
+    constructor(private postService: PostService,
+                private domainService: DomainService,
+                public globals: GlobalsService) {
     }
-    this.postService.updatePost(post);
-    console.log(post);
-  }
 
-  getDomain(domainName) {
-    this.domainService.getDomain(domainName).subscribe( (domain) => {
-      console.log(domainName);
-      console.log(domain);
-      this.domain = domain;
-    });
-  }
+
+    ngOnInit() {
+        console.log(this.post);
+        this.getDomain(this.post.Domain);
+    }
+
+    updateRank(post: Post, up: boolean) {
+        if (up) {
+            if (this.hasVoteChanged) {
+                post.Rank += 1;
+                this.hasVoteChanged = false;
+                this.voteStatus = 'black';
+            } else {
+                post.Rank += 1;
+                this.hasVoteChanged = true;
+                this.voteStatus = 'green';
+            }
+        } else {
+            if (this.hasVoteChanged) {
+                post.Rank -= 1;
+                this.hasVoteChanged = false;
+                this.voteStatus = 'black';
+            } else {
+                post.Rank -= 1;
+                this.hasVoteChanged = true;
+                this.voteStatus = 'red';
+            }
+
+        }
+        this.postService.updatePost(post);
+        console.log(post);
+    }
+
+    getDomain(domainName) {
+        this.domainService.getDomain(domainName).subscribe((domain) => {
+            console.log(domainName);
+            console.log(domain);
+            this.domain = domain;
+        });
+    }
 }
