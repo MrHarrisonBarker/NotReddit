50f4ee2f32029f7f6cca7da85ae0f3e5da02b14d
Harrison Barker <harrison@thebarkers.me.uk>
2018-10-12 16:50:08 +0100

    docker compose file edits, shitty broken error handling for duplicate domain adding ( needs to be fixed )

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,54 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {DomainService} from '../domain.service';
 import {Domain} from '../domain';
 
 @Component({
-  selector: 'app-start',
-  templateUrl: './start.component.html',
-  styleUrls: ['./start.component.scss']
+    selector: 'app-start',
+    templateUrl: './start.component.html',
+    styleUrls: ['./start.component.scss']
 })
 export class StartComponent implements OnInit {
 
-  startForm: FormGroup;
-  contentTypes = ['images', 'links', 'text', 'all'];
+    startForm: FormGroup;
+    contentTypes = ['images', 'links', 'text', 'all'];
+    domains: Domain[];
+    displayError: Boolean;
 
-  constructor(private formBuilder: FormBuilder,
-              private domainService: DomainService) {
-    this.startForm = formBuilder.group({
-      'Title':  ['', Validators.required ],
-      'Name': ['', Validators.required],
-      'Topic': ['', Validators.required ],
-      'Description': [''],
-      'Colour': [''],
-      'Content': [''],
-      'Icon': ['']
-    });
-  }
+    constructor(private formBuilder: FormBuilder,
+                private domainService: DomainService) {
+        this.startForm = formBuilder.group({
+            'Title': ['', Validators.required],
+            'Name': ['', Validators.required],
+            'Topic': ['', Validators.required],
+            'Description': [''],
+            'Colour': [''],
+            'Content': [''],
+            'Icon': ['']
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.displayError = false;
+        this.getAllDomains();
+    }
 
-  startDomain(domain: Domain) {
-    this.domainService.startDomain(domain);
-    console.log(domain);
-  }
+    startDomain(domain: Domain) {
+        this.domainService.startDomain(domain);
+        console.log(domain);
+    }
 
-  submitForm() {
-    const domain = new Domain();
-    const submittedDomain = this.startForm.value;
+    getAllDomains() {
+        this.domainService.getAllDomains().subscribe(domains => this.domains = domains);
+    }
 
-    domain.Title = submittedDomain.Title;
-    domain.Name = submittedDomain.Name;
-    domain.Topic = submittedDomain.Topic;
-    domain.Description = submittedDomain.Description;
-    domain.Colour = submittedDomain.Colour;
-    domain.Content = submittedDomain.Content;
-    domain.Icon = submittedDomain.Icon;
+    submitForm() {
+        const domain = new Domain();
+        const submittedDomain = this.startForm.value;
 
-    domain.Subscribers = 0;
+        this.domains.forEach(publicDomain => {
+            if (submittedDomain.Name === publicDomain.Name) {
+                console.log('error: name exists');
+                this.displayError = true;
+                return error;
+            }
+        });
 
-    this.startDomain(domain);
-  }
+        domain.Title = submittedDomain.Title;
+        domain.Name = submittedDomain.Name;
+        domain.Topic = submittedDomain.Topic;
+        domain.Description = submittedDomain.Description;
+        domain.Colour = submittedDomain.Colour;
+        domain.Content = submittedDomain.Content;
+        domain.Icon = submittedDomain.Icon;
+
+        domain.Subscribers = 0;
+
+        this.startDomain(domain);
+    }
 
 }
